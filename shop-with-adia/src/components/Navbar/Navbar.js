c83a5cb1a96648038ab9d19e55c4b11ba38b0534
darsuabasi <abasibasiurua@gmail.com>
2021-01-23 13:32:00 -0500

    create basic vertical navbar skeleton + css

---
+++ shop-with-adia/src/components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import './navbar.css'
+
+
+const Navbar = () => {
+    return (
+        <div class="sidebar" className="main_navbar_container">
+
+            <ul className="navbar_ui">
+                <li class="navbar_li">
+                    <NavLink className="navigation-links" to={"/"}> Home </NavLink>
+                </li>
+
+                <li class="navbar_li">
+                    <NavLink className="navigation-links" to={"/adiah"}> Adia </NavLink>
+                </li>
+
+                <li class="navbar_li">
+                    <NavLink className="navigation-links" to={"/products"}> Products </NavLink>
+                </li>
+
+
+                <li class="navbar_li">
+                    <NavLink className="navigation-links" to={"/shop"}> Shop </NavLink>
+                </li>
+
+                <li class="navbar_li">
+                    <NavLink className="navigation-links" to={"/explore"}> Explore </NavLink>
+                </li>
+            </ul>
+        </div> 
+    )
+}
+
+export default  Navbar;
