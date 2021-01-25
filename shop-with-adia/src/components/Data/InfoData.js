c07c91b39ea0ec66f8e70030f231f521e01e4f9a
darsuabasi <abasibasiurua@gmail.com>
2021-01-24 19:56:05 -0500

    create data file for info section

---
+++ shop-with-adia/src/components/Data/InfoData.js
@@ -0,0 +1,79 @@
+import fragrances from '../../infoDataAssets/fragrances-infodata.jpg';
+import books from '../../infoDataAssets/books-infodata.jpg';
+import skincare from '../../infoDataAssets/skincare-infodata.jpg';
+import makeup from '../../infoDataAssets/makeup-infodata.jpg';
+import wine from '../../infoDataAssets/wine-infodata.jpg';
+import fashion from '../../infoDataAssets/fashion-infodata.jpg';
+
+
+export const InfoData = [
+    {
+        heading: 'Beauty',
+        paragraphOne: 'this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph',
+        paragraphTwo: 'this is the SECOND ever paragraph. this is the SECOND ever paragraph. this is the SECOND ever paragraph.',
+        buttonLabel: 'Shop Fragrances',
+        image: fragrances,
+        reverse: false,
+        delay: 100,
+    }
+]
+
+export const InfoDataTwo = [
+    {
+        heading: 'Books',
+        paragraphOne: 'this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph',
+        paragraphTwo: 'this is the SECOND ever paragraph. this is the SECOND ever paragraph. this is the SECOND ever paragraph.',
+        buttonLabel: 'Shop Fragrances',
+        image: books,
+        reverse: true,
+        delay: 200,
+    }
+]
+
+export const InfoDataThree = [
+    {
+        heading: 'Makeup',
+        paragraphOne: 'this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph',
+        paragraphTwo: 'this is the SECOND ever paragraph. this is the SECOND ever paragraph. this is the SECOND ever paragraph.',
+        buttonLabel: 'Shop Fragrances',
+        image: makeup,
+        reverse: false,
+        delay: 300,
+    }
+]
+
+export const InfoDataFour = [
+    {
+        heading: 'Wine',
+        paragraphOne: 'this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph',
+        paragraphTwo: 'this is the SECOND ever paragraph. this is the SECOND ever paragraph. this is the SECOND ever paragraph.',
+        buttonLabel: 'Shop Fragrances',
+        image: wine,
+        reverse: true,
+        delay: 100,
+    }
+]
+
+export const InfoDataFive = [
+    {
+        heading: 'Skincare',
+        paragraphOne: 'this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph',
+        paragraphTwo: 'this is the SECOND ever paragraph. this is the SECOND ever paragraph. this is the SECOND ever paragraph.',
+        buttonLabel: 'Shop Fragrances',
+        image: skincare,
+        reverse: false,
+        delay: 200,
+    }
+]
+
+export const InfoDataSix = [
+    {
+        heading: 'Fashion',
+        paragraphOne: 'this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph. this is the first ever paragraph',
+        paragraphTwo: 'this is the SECOND ever paragraph. this is the SECOND ever paragraph. this is the SECOND ever paragraph.',
+        buttonLabel: 'Shop Fragrances',
+        image: fashion,
+        reverse: true,
+        delay: 300,
+    }
+]
