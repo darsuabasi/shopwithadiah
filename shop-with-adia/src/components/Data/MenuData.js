004b5840faea42dc29eca72a799e5125152d1ee1
darsuabasi <abasibasiurua@gmail.com>
2021-01-24 19:50:11 -0500

    add navbar data

---
+++ shop-with-adia/src/components/Data/MenuData.js
@@ -0,0 +1,6 @@
+export const menuData = [
+    {title: 'Home', link: '/'},
+    {title: 'Adiah', link: '/adiah'},
+    {title: 'Explore', link: '/adiah/explore'},
+    {title: 'Shop', link: '/adiah/shop'},
+]
