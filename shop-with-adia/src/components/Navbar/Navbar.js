716eec77ec44886bf21b8b63e35d9617b9a9e68e
darsuabasi <abasibasiurua@gmail.com>
2021-01-30 04:11:36 -0500

    create add update cart functionality in nav

--- shop-with-adia/src/components/Navbar/Navbar.js
+++ shop-with-adia/src/components/Navbar/Navbar.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styled, { css} from 'styled-components/macro';
 import { Button } from '../Button/Button';
 import { menuData } from '../Data/MenuData';
 import { FaBars } from 'react-icons/fa';
+import { GiShoppingCart } from 'react-icons/gi';
+// import { useSelector, useDispatch } from "react-redux";
+// import { useStateValue } from "../../StateProvider";
+import { useSelector, useDispatch } from "react-redux"
+
 
 const Nav = styled.nav`
-    height: 12vh;
+    height: 11vh;
     display: flex;
     justify-content: space-between;
     padding: 1rem 2rem;
     z-index: 100;
     position: fixed;
     width: 100%;
-    ${'' /* background: #000; */}
+    background: #000;
     text-align: center;
     ${'' /* margin-top: 1%; */}
 
     @media screen and (max-width: 768px) {
         display: flex;
-        height: 12vh;
+        height: 11vh;
         width: 100vw;
         float: none;
     }
@@ -66,6 +71,41 @@ const MenuBars = styled(FaBars)`
     }
 `;
 
+const ShoppingCart = styled(GiShoppingCart)`
+    height: 2rem;
+    width: 2rem;
+
+    @media screen and (max-width: 768px) {
+        display: flex;
+        background-size: contain;
+        height: 40px;
+        width: 40px;
+        cursor: pointer;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-50%, 5 5%);
+        margin-top: 3.5%
+    }
+`;
+
+const BasketDiv = styled.div`
+    display: flex;
+    align-items: center;
+    color: white;
+    
+    @media screen and (max-width: 768px) {
+        ${'' /* display: none; */}
+    }
+`
+
+const SpanBasket = styled.span `
+    margin-left: 3px;
+    margin-right: 3px;
+    font-size: 18px;
+    font-weight: 800;
+`
+
 const NavMenu = styled.div`
     display: flex; 
     align-items: center;
@@ -86,15 +126,41 @@ const NavMenuLinks = styled(Link)`
 const NavButton = styled.div`
     display: flex;
     align-items: center;
-    margin-right: 24px;
+    margin-right: 4px;
 
     @media screen and (max-width: 768px) {
         display: none;
     }
+`;
+
+const NavButtonTwo = styled.div`
+    display: flex;
+    align-items: center;
+    ${'' /* margin-right: 24px; */}
+
+`;
+
+const BasketButton  = styled(Link) `
+    background: ${({ primary }) => (primary ? '#fff' : 'CD853F')};
+    white-space: nowrap;
+    outline: none;
+    border: none;
+    min-width: 100px;
+    max-width: 200px;
+    cursror: pointer;
+    text-decoration: none;
+    transition: 0.4s;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: ${({big}) => (big ? '16px 40px' : '14px 24px')}; 
+    color: ${({ primary }) => (primary ? '#000' : '000d1a')};
+    font-size: ${({ big }) => (big ? '20px' : '14px')}; 
 `
 
+const Navbar = ({toggle, totalItems}) => {
+    const location = useLocation();
 
-const Navbar = ({toggle}) => {
     return (
         <Nav class="sidebar" className="main_navbar_container">
             <Logo to="/"> Adiah Logo </Logo>
@@ -102,7 +168,6 @@ const Navbar = ({toggle}) => {
             <MenuBars onClick={toggle}/>
 
             <NavMenu>
-
                 {menuData.map((item, index) => (
                     <NavMenuLinks to={item.link} key={index}>
                         {item.title}
@@ -111,31 +176,20 @@ const Navbar = ({toggle}) => {
             </NavMenu>
 
             <NavButton> 
-                    <Button to='/contact' primary="true">
-                        Contact Me
-                    </Button>
-                    
-
+                <Button to='/contact' primary="true">
+                    Contact Me
+                </Button>
+                {/* {location.pathname === '/adiah/shop/cart'  && ( */}
+                    <BasketButton to='/adiah/shop/cart'>
+                        <BasketDiv>
+                            <ShoppingCart/>
+                            <SpanBasket>
+                                {totalItems}
+                            </SpanBasket>
+                        </BasketDiv>
+                    </BasketButton>
+                {/* )} */}
             </NavButton>
-
-            {/* <ul className="navbar_ui">
-                <li class="navbar_li">
-                    <NavLink className="navigation-links" to={"/"}> Home </NavLink>
-                </li>
-
-                <li class="navbar_li">
-                    <NavLink className="navigation-links" to={"/adiah"}> Adiah </NavLink>
-                </li>
-
-                <li class="navbar_li">
-                    <NavLink className="navigation-links" to={"/adiah/explore"}> Explore </NavLink>
-                </li>
-
-
-                <li class="navbar_li">
-                    <NavLink className="navigation-links" to={"/adiah/shop"}> Shop </NavLink>
-                </li>
-            </ul> */}
         </Nav> 
     )
 }
