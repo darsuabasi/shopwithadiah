adc68623a457a3eaa8c0b0b7b39c487f670d994c
darsuabasi <abasibasiurua@gmail.com>
2021-02-16 04:04:21 -0500

    create address form with styling

---
+++ shop-with-adia/src/components/CheckoutForm/AddressForm.js
@@ -0,0 +1,158 @@
+import React, { useState, useEffect } from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import { Link, useHistory } from 'react-router-dom';
+import styled from 'styled-components/macro';
+import FormInput from './CustomTextField';
+import commerce from './../../lib/commerce';
+
+const AddressFormMain = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 1400px;
+`;
+
+// .MuiStepIcon-root.MuiStepIcon-active {
+//     color: #f8d7c4;
+// }
+// .MuiStepIcon-root.MuiStepIcon-completed {
+//     color: #f8d7c4;
+// }
+
+// const InputLabel = styled.label`
+   
+// `;
+
+// const Select = styled.div`
+   
+// `;
+
+// const MenuItem = styled.div`
+   
+// `;
+
+// const Button = styled(Link)`
+   
+// `;
+
+// const Grid = styled.div`
+   
+// `;
+
+
+const AddressForm = ({ checkoutToken, next }) => {
+  const [shippingCountries, setShippingCountries] = useState([]);
+  const [shippingCountry, setShippingCountry] = useState('');
+  const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+  const [shippingSubdivision, setShippingSubdivision] = useState('');
+  const [shippingOptions, setShippingOptions] = useState([]);
+  const [shippingOption, setShippingOption] = useState('');
+  // const [taxOptions, setTaxOptions] = useState([]);
+  // const [taxOption, setTaxOption] = useState('');
+  const methods = useForm();
+  const history = useHistory();
+
+  const fetchShippingCountries = async (checkoutTokenId) => {
+    const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
+    setShippingCountries(countries);
+    setShippingCountry(Object.keys(countries)[0]);
+  };
+
+  const fetchSubdivisions = async (countryCode) => {
+    const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode);
+    setShippingSubdivisions(subdivisions);
+    setShippingSubdivision(Object.keys(subdivisions)[0]);
+  };
+
+  const fetchShippingOptions = async (checkoutTokenId, country, stateProvince = null) => {
+    const options = await commerce.checkout.getShippingOptions(checkoutTokenId, { country, region: stateProvince });
+    setShippingOptions(options);
+    setShippingOption(Object.keys(options)[0]);
+    // setShippingOption(options[0].id);
+  };
+
+  // const fetchTaxOptions = async (checkoutTokenId, country, stateProvince = null) => {
+  //   const taxes = await commerce.checkout.setTaxZone(checkoutTokenId, { country, region: stateProvince });
+  //   setTaxOptions(taxes);
+  //   setTaxOption((taxes)[0]);
+  // };
+
+  useEffect(() => {
+    fetchShippingCountries(checkoutToken.id);
+  }, []);
+
+  useEffect(() => {
+    if (shippingCountry) fetchSubdivisions(shippingCountry);
+  }, [shippingCountry]);
+
+  useEffect(() => {
+    if (shippingSubdivision) fetchShippingOptions(checkoutToken.id, shippingCountry, shippingSubdivision);
+  }, [shippingSubdivision]);
+
+  // useEffect(() => {
+  //   if (shippingSubdivision) fetchTaxOptions(checkoutToken.id, shippingCountry, shippingSubdivision);
+  // }, [shippingSubdivision]);
+
+  return (
+    <>
+      <Typography variant='h6' gutterBottom>Shipping Address</Typography>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit((data) => next({ ...data, shippingCountry, shippingSubdivision, shippingOption }))}>
+          <Grid container spacing={3}>
+            <FormInput required name="firstName" label="First name" />
+            <FormInput required name="lastName" label="Last name" />
+            <FormInput required name="address1" label="Address line 1" />
+            <FormInput required name="email" label="Email" />
+            <FormInput required name="city" label="City" />
+            <FormInput required name="zip" label="Zip / Postal code" />
+
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                {Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name })).map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+
+
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Subdivision</InputLabel>
+              <Select value={shippingSubdivision} fullWidth onChange={(e) => setShippingSubdivision(e.target.value)}>
+                {Object.entries(shippingSubdivisions).map(([code, name]) => ({ id: code, label: name })).map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+
+
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Options</InputLabel>
+              <Select value={shippingOption} fullWidth onChange={(e) => setShippingOption(e.target.value)}>
+                {shippingOptions.map((sO) => ({ id: sO.id, label: `${sO.description} - (${sO.price.formatted_with_symbol})` })).map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+
+
+          </Grid>
+          <br />
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Button variant="outlined" onClick={() => history.push('/adiah/shop/cart')}>Back to Cart</Button>
+            <Button type="submit" variant="contained" style={{background:"#f8d7c4"}}>Next</Button>
+          </div>
+        </form>
+      </FormProvider>
+    </>
+  );
+};
+export default AddressForm;
